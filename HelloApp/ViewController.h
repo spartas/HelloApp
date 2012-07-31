7684861d31fa1a78b6e38f6cccfa9f2bd8cfaf10
Tim Wright <spartas@gmail.com>
2012-07-31 13:40:37 -0500

    Initial Commit

---
+++ HelloApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloApp
+//
+//  Created by Timothy Wright on 7/31/12.
+//  Copyright (c) 2012 Timothy Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
